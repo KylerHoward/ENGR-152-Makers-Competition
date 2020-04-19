6636ed5d5c07192c13fdfb925dda903658bcc808
JacksonPope <63939687+JacksonPope@users.noreply.github.com>
2020-04-18 23:22:12 -0500

    Update Remind_Device.ino

--- Remind_Device/Remind_Device.ino
+++ Remind_Device/Remind_Device.ino
@@ -1,7 +1,7 @@
 /* ENGR 152
  *  Maker Competition
  *  Remind Device
- *  Last edited by Kyler Howard, 4-17-20
+ *  Last edited by Jackson Pope, 4-18-20
 */
 
 //================================================================================
@@ -17,29 +17,32 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 // sets constant state variables from 1 to 7
 const int STATE_GENERAL_WAIT = 1;
-
 const int STATE_LCD_ON = 2;
-
 const int STATE_FILE_WAIT = 3;
-
 const int STATE_AUDIO_SELECT = 4;
-
 const int STATE_SPEAKER_PLAY = 5;
-
 const int STATE_ACKNOWLEDGE_WAIT = 6;
-
 const int STATE_MOTION_OFF = 7;
 
-// setting other variables
-int IndicatorLED = 8;
-int ConfirmationButton = 9;
-int ConfirmationButtonState = 1;
 
-// TO DO: button state variable, joystick state variable, motion sensor state variable, speaker count variable, and whatever WAVE needs
+// declaring digital pins
+int IndicatorLED = 8;            // Indicator LED output
+int ConfirmationButton = 9;      //acknowledge button? Input
+int MotionSensor = 10;           // Motion sensor input
 
-// sets the currentState to STATE_WELCOME
+// declaring analog pins
+int XJoystick = A0;              // x-axis of joystick
+int YJoystick = A1;              // y-axis of joystick
+int JoystickButton = A2;         // joystick button treat as digital
 
-int currentState = 1;
+// declare variable data holders
+int SpeakerCount;                // For WAVE shield  -  not sure what it does
+int ConfirmationButtonState;     // Do we need button state? could it just be in an if statement?
+int CurrentState;                // Holds State of State machine
+
+// Custom Function Variable Parameters
+int DebounceDelay = 500;         // Delay set for debounce time - delay time after a button is pressed
+
 //================================================================================
 //                         Main Initializing Function
 //================================================================================
@@ -52,25 +55,27 @@ void setup() {
   // clearing the LCD and putting the cursor at 0,0
   lcd.clear();
 
-
   // start serial monitor at 9600 bpm
   Serial.begin(9600);
 
-  // set the currentState to STATE_WELCOME
-  currentState = STATE_GENERAL_WAIT;
+  // set the currentState to STATE_GENERAL_WAIT
+  CurrentState = STATE_GENERAL_WAIT;
 
-  // MAKE THE LED TURN ON
+  // Set pin modes for digital pins
   pinMode (IndicatorLED, OUTPUT);
+  pinMode (ConfirmationButton, INPUT_PULLUP);    // Logic is INVERTED!
+  pinMode (MotionSensor, INPUT_PULLUP);          // Logic is INVERTED!
+  
+  // Set pin modes for analog pins
+  pinMode (XJoystick, INPUT);
+  pinMode (YJoystick, INPUT);
+  pinMode (JoystickButton, INPUT);
 
+  //Turn on indicator LED
   digitalWrite (IndicatorLED, HIGH);
 
-  pinMode (ConfirmationButton, INPUT_PULLUP);
-
-
-
+  // Print initializing message on LCD Screen
   lcd.print("~~TESTING~~");
-  
-  
 }
 
 
@@ -80,30 +85,68 @@ void setup() {
 
 void loop() { 
 
+  // State machine will Print out which state it moves to. Helps with troubleshooting
   // Starts state machine
   switch(currentState){
   
+      
     //=============================
     //    State One
     //=============================
 
     // starts state one of state machine
     case STATE_GENERAL_WAIT:
-
-      // If the joystick or button is pressed, move to STATE_LCD_ON
-      // If the motion detector is sensed, set the speaker counter to 1 and move to STATE_SPEAKER_PLAY
       
-      ConfirmationButtonState = digitalRead(ConfirmationButton);
+      // Print trouble shooting information 
+        // If button is pressed, print button pushed. - note: logic is inverted because of pullup input
+        if (digitalRead(ConfirmationButton) == LOW){
+          Serial.println("Button Pushed");
+        }
       
-      if (ConfirmationButtonState == LOW){
+        // If Joystick is moved, Print the analog value of Joystick inputs 
+        if(XJoystick > 700 || XJoystick < 300 || YJoystick > 700 || YJoystick < 300) {
+          
+          // Print "Joystick moved"
+          Serial.println("Joystick Moved");
+          
+          //print x-axis: (analog reading of x)
+          Serial.print("x-axis: ");
+          Serial.println(XJoystick);
+          
+          //print y-axis: (analog reading of y)
+          Serial.print("y-axis: ");
+          Serial.println(YJoystick);
+        }
 
-        Serial.print("Button Pushed");
+
+      // If the joystick is moved or button is pressed, move to STATE_LCD_ON
+      if(XJoystick > 700 || XJoystick < 300 || YJoystick > 700 || YJoystick < 300 || JoystickButton == HIGH) {
+       
+        // move to STATE_LCD_ON
+        CurrentState = STATE_LCD_ON;
+        Serial.Println("STATE_LCD_ON");
+        
+        Debounce();
       }
+      
+      // If motion is sensed, set the speaker counter to 1 and move to STATE_SPEAKER_PLAY
+      // Logic is reversed for pullup input
+      if(MotionSensor==LOW){
+        
+        //Set speaer counter to 1
+        SpeakerCounter = 1;
+        
+        //Go to STATE_SPEAKER_PLAY
+        CurrentState = STATE_SPEAKER_PLAY;
+        Serial.Println("STATE_SPEAKER_PLAY");
+        
+        Debounce();
+      }      
 
       // end the case
       break;
 
-
+      
     //=============================
     //    State Two
     //=============================
@@ -193,3 +236,12 @@ void loop() {
   }
     
 }                                    // Go back to the beginning of the loop
+
+
+//===========================================
+//             Custom Functions
+//===========================================
+
+void Debounce(void) {
+  delay(DebounceDelay);
+}
