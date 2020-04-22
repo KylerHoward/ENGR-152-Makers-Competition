f8ccceaf06d95e043118f568e8f9b7c38f4123c6
Kyler Howard <kylerhoward@gmail.com>
2020-04-22 15:03:08 -0700

    Added a header to have Wave setup custom function and possibly more

---
+++ Remind_Device/remind.cpp
@@ -0,0 +1,75 @@
+/*
+ * Custom Wave Shield Function Library
+ * Last edited by Kyler Howard and Jackson Pope
+ */
+
+
+//=================================================================
+// Include Libraries & Declare Global Variables
+// ================================================================
+
+#include "Arduino.h"
+#include "remind.h"
+#include <WaveHC.h>
+#include <WaveUtil.h>
+
+SdReader card;    // This object holds the information for the card
+FatVolume vol;    // This holds the information for the partition on the card
+FatReader root;   // This holds the information for the volumes root directory
+WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
+
+uint8_t dirLevel; // indent level for file/dir names    (for prettyprinting)
+dir_t dirBuf;     // buffer for directory reads
+
+
+/*
+ * Define macro to put error messages in flash memory
+ */
+#define error(msg)
+
+
+//=================================================================
+// Custom Function Definitions
+// ================================================================
+
+// Function for setting the debounce delay time for the buttons library
+void waveSetup(void)
+{
+  putstring("Free RAM: ");       // This can help with debugging, running out of RAM is bad
+  Serial.println(FreeRam());
+
+  //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
+  if (!card.init()) {         //play with 8 MHz spi (default faster!)  
+    error("Card init. failed!");  // Something went wrong, lets print out why
+  }
+  
+  // enable optimize read - some cards may timeout. Disable if you're having problems
+  card.partialBlockRead(true);
+  
+  // Now we will look for a FAT partition!
+  uint8_t part;
+  for (part = 0; part < 5; part++) {   // we have up to 5 slots to look in
+    if (vol.init(card, part)) 
+      break;                           // we found one, lets bail
+  }
+  if (part == 5) {                     // if we ended up not finding one  :(
+    error("No valid FAT partition!");  // Something went wrong, lets print out why
+  }
+  
+  // Lets tell the user about what we found
+  putstring("Using partition ");
+  Serial.print(part, DEC);
+  putstring(", type is FAT");
+  Serial.println(vol.fatType(), DEC);     // FAT16 or FAT32?
+  
+  // Try to open the root directory
+  if (!root.openRoot(vol)) {
+    error("Can't open root dir!");      // Something went wrong,
+  }
+  
+  // Whew! We got past the tough parts.
+  putstring_nl("Files found (* = fragmented):");
+
+  // Print out all of the files in all the directories.
+  root.ls(LS_R | LS_FLAG_FRAGMENTED);
+}
---
+++ Remind_Device/remind.h
@@ -0,0 +1,18 @@
+/*
+ * *******************************************
+ * 
+ *  Remind Library for Wave Shield Functions
+ * 
+ * *******************************************
+ */
+
+
+// This is the format needed for the header file
+#ifndef REMIND_H
+
+#define REMIND_H
+
+// Declare functions that will be defined in the .cpp file
+void waveSetup(void);
+
+#endif /* REMIND_H */
