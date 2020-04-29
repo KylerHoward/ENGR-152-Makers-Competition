bb3b7ad69cd652e3902bedf4be1225fbe1d53308
Kyler Howard <kylerhoward@gmail.com>
2020-04-28 21:28:57 -0700

    Got the array to change size based on number of files on SD

---
+++ Wave_Shield_Menu_Test/remind.cpp
@@ -0,0 +1,144 @@
+/*
+ * Custom Wave Shield Function Library
+ * Last edited by Kyler Howard and Jackson Pope
+ */
+
+
+//=================================================================
+// Include Libraries & Declare Global Variables
+// ================================================================
+
+#include <WaveHC.h>
+#include <WaveUtil.h>
+#include <FatStructs.h>
+#include <FatReader.h>
+#include "Arduino.h"
+#include "remind.h"
+
+
+SdReader card;    // This object holds the information for the card
+FatVolume vol;    // This holds the information for the partition on the card
+FatReader root;   // This holds the information for the volumes root directory
+FatReader f;      // This holds the information for the file we're play
+WaveHC wave;      // This is the only wave (audio) object, since we will only play one at a time
+
+uint8_t dirLevel; // indent level for file/dir names    (for prettyprinting)
+dir_t dirBuf;     // buffer for directory reads
+
+
+/*
+ * Define macro to put error messages in flash memory
+ */
+#define error(msg)
+
+
+//=================================================================
+// Custom Function Definitions
+// ================================================================
+
+// Function for setting the debounce delay time for the buttons library
+FatReader waveSetup(void)
+{
+  Serial.print("Free RAM: ");       // This can help with debugging, running out of RAM is bad
+  Serial.println(FreeRam());
+
+  //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
+  if (!card.init()) {         //play with 8 MHz spi (default faster!)  
+    error("Card init. failed!");  // Something went wrong, lets print out why
+  }
+  
+  // enable optimize read - some cards may timeout. Disable if you're having problems
+  card.partialBlockRead(true);
+  
+  // Now we will look for a FAT partition!
+  uint8_t part;
+  for (part = 0; part < 5; part++) {   // we have up to 5 slots to look in
+    if (vol.init(card, part)) 
+      break;                           // we found one, lets bail
+  }
+  if (part == 5) {                     // if we ended up not finding one  :(
+    error("No valid FAT partition!");  // Something went wrong, lets print out why
+  }
+  
+  // Lets tell the user about what we found
+  Serial.print("Using partition ");
+  Serial.print(part, DEC);
+  Serial.print(", type is FAT");
+  Serial.println(vol.fatType(), DEC);     // FAT16 or FAT32?
+  
+  // Try to open the root directory
+  if (!root.openRoot(vol)) {
+    error("Can't open root dir!");      // Something went wrong,
+  }
+  
+  // Whew! We got past the tough parts.
+  Serial.println("Files found (* = fragmented):");
+
+  // Print out all of the files in all the directories.
+  root.ls(LS_R | LS_FLAG_FRAGMENTED);
+
+  root.rewind();
+
+  return root;
+
+}
+
+// Plays a full file from beginning to end with no pause.
+void playcomplete(char *name) {
+  // call our helper to find and play this name
+  playfile(name);
+  while (wave.isplaying) {
+  // do nothing while its playing
+  }
+  // now its done playing
+}
+
+
+void playfile(char *name) {
+  // see if the wave object is currently doing something
+  if (wave.isplaying) {// already playing something, so stop it!
+    wave.stop(); // stop it
+  }
+  // look in the root directory and open the file
+  if (!f.open(root, name)) {
+    Serial.print("Couldn't open file "); 
+    Serial.println(name); 
+    return;
+  }
+  // OK read the file and turn it into a wave object
+  if (!wave.create(f)) {
+    Serial.println("Not a valid WAV"); 
+    return;
+  }
+  
+  // ok time to play! start playback
+  wave.play();
+}
+
+uint32_t getFiles(FatReader &dir) {
+  FatReader file;
+  int i = 0;
+  Serial.println("Starting Count");
+  while (dir.readDir(dirBuf) > 0) {    // Read every file in the directory one at a time
+  
+    // Skip it if not a subdirectory and not a .WAV file
+    if (!DIR_IS_SUBDIR(dirBuf)
+         && strncmp_P((char *)&dirBuf.name[8], PSTR("WAV"), 3)) {
+      continue;
+    }
+
+    if (!file.open(vol, dirBuf)) {        // open the file in the directory
+      error("file.open failed");          // something went wrong
+    }
+
+    if (file.isDir()) {                   // check if we opened a new directory
+      continue;
+    }
+    
+    i++;
+    Serial.println((char *)&dirBuf.name);
+    
+    
+  }
+  return i;
+}
---
+++ Wave_Shield_Menu_Test/remind.h
@@ -0,0 +1,26 @@
+/*
+ * *******************************************
+ * 
+ *  Remind Library for Wave Shield Functions
+ * 
+ * *******************************************
+ */
+
+
+// This is the format needed for the header file
+#ifndef REMIND_H
+
+#define REMIND_H
+
+// Declare functions that will be defined in the .cpp file
+FatReader waveSetup(void);
+
+// Plays a full file from beginning to end with no pause.
+void playcomplete(char *name);
+
+void playfile(char *name);
+
+uint32_t getFiles(FatReader &dir);
+
+
+#endif /* REMIND_H */
